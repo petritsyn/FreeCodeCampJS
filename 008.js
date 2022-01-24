6c4fea9e57ade33cbc91406920841773efec2630
youngvvp <andrey2343@yandex.ru>
2022-01-24 23:34:24 +0300

    008

---
+++ 008.js
@@ -0,0 +1,9 @@
+titleCaseOver// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
